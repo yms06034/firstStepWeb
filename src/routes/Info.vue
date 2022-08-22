ae016fe40152b7a78f32d3f8bdeb81effa748b77
Sorgfalt <yeongjun1324@gmail.com>
2022-08-22 22:22:38 +0900

    doing info page

--- src/routes/Info.vue
+++ src/routes/Info.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>Info</h1>
+	<form>
+		<article>
+			<div>
+				<h1>TRAVEL INFORMATION</h1>
+			</div>
+			<div>
+				<input type="button" class="p_btn" value="#All"/>
+				<input type="button" class="p_btn" value="#국내/제주"/>
+				<input type="button" class="p_btn" value="#동남아"/>
+				<input type="button" class="p_btn" value="#일본"/>
+				<input type="button" class="p_btn" value="#남태평양"/>
+				<input type="button" class="p_btn" value="#극동러시아/중국"/>
+				<input type="button" class="p_btn" value="#유럽"/>
+				<input type="button" class="p_btn" value="#북미/중남미"/>
+			</div>
+		</article>
+	</form>
 </template>
+
+<style lang="scss" scoped>
+	@import "~/scss/main";
+</style>
