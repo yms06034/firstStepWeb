fc365b4397cc6958feb2d12fb00d2bdc199c8ded
Kim Sung Jin <yms06034@gmail.com>
2022-08-24 22:09:40 +0900

    header footer add

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Header />
   <RouterView />
+  <Footer />
 </template>
 
 <script>
 import Header from '~/components/Header'
+import Footer from '~/components/Footer'
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
