dd4c80682b90e5df689985c25cc15ce306e6d468
Sorgfalt <yeongjun1324@gmail.com>
2022-08-31 21:50:38 +0900

    change to table list

--- src/routes/Cmnty.vue
+++ src/routes/Cmnty.vue
@@ -30,8 +30,14 @@
 							<th v-for = "item in header" :key="item">{{ item }}</th>
 					</thead>
 					<tbody>
-						<tr v-for="line in ranking" :key="line">
-							<td v-for="item in line" :key="item">{{ item }}</td>
+						<tr>
+							<td v-for="item in List" :key="item">{{ item.no }}</td>
+							<td v-for="item in List" :key="item">{{ item.category }}</td>
+							<td v-for="item in List" :key="item">{{ item.title }}</td>
+							<td v-for="item in List" :key="item">{{ item.regId }}</td>
+							<td v-for="item in List" :key="item">{{ item.regDate }}</td>
+							<td v-for="item in List" :key="item">{{ item.views }}</td>
+							<td v-for="item in List" :key="item">{{ item.likes }}</td>
 						</tr>
 					</tbody>
 				</table>
@@ -45,13 +51,16 @@
 		data() {
 			return {
 				header: ["번호","","제목","글쓴이","작성일","조회수","추천수"] ,
-				ranking:[
-						[6,"# 게임","띠부","admin","04:20",100, 1000],
-						[5,"# 게임","띠부","admin","04:20",100, 1000],
-						[4,"# 게임","띠부","admin","04:20",100, 1000],
-						[3,"# 게임","띠부","admin","04:20",100, 1000],
-						[2,"# 게임","띠부","admin","04:20",100, 1000],
-						[1,"# 게임","띠부","admin","04:20",100, 1000],
+				List:[
+						{
+							no : 10
+							, category : "# 게임"
+							, title : "띠부띠부"
+							, regId : "admin"
+							, regDate : "04-22"
+							, views : 100
+							, likes : 120
+						}
 				]
 			}
 		},
@@ -70,16 +79,17 @@
 				width: 1260px;
 				margin: 0 auto;
 				border : 1px solid #eeeeef;
-				}
-	table th{
+		th{
 			padding : 12px;
 			border-top: 1px solid  gray;
-	}
-	table thead{
+		}
+		thead{
 		border-bottom: 1px solid gray;
+		}
+		td{
+				padding : 12px;
+				border-bottom: 1px solid gray;
+		}
 	}
-	table td{
-			padding : 12px;
-			border-bottom: 1px solid gray;
-	}
+	
 </style>
