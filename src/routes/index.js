a1a1d60774289fb4df9ad81712537f2170bdf393
Kim Sung Jin <yms06034@gmail.com>
2022-09-23 16:48:02 +0900

    Reset scrolling when moving a page

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,32 +10,19 @@ import CmntyComponent from './Cmnty'
 import NoticeComponent from './Notice'
 
 export default createRouter({
-    history: createWebHashHistory(),
-    routes: [
-        { path: '/', component: HomeComponent , meta: { scrollToTop: true }},
-        { path: '/Info', component: InfoComponent , meta: { scrollToTop: true }},
-        { path: '/Info_Detail', component: Info_DetailComponent , meta: { scrollToTop: true }},
-        { path: '/Info_Post', component: Info_PostComponent , meta: { scrollToTop: true }},
-        { path: '/Review', component: ReviewComponent , meta: { scrollToTop: true }},
-        { path: '/Review_Detail', component: Review_DetailComponent , meta: { scrollToTop: true }},
-        { path: '/Review_Post', component: Review_PostComponent , meta: { scrollToTop: true }},
-        { path: '/Cmnty', component: CmntyComponent , meta: { scrollToTop: true }},
-        { path: '/Notice', component: NoticeComponent , meta: { scrollToTop: true }},
-    ]
+  history: createWebHashHistory(),
+  scrollBehavior(){
+      return { top: 0 }
+  },
+  routes: [
+    { path: '/', component: HomeComponent , meta: { scrollToTop: true }},
+    { path: '/Info', component: InfoComponent , meta: { scrollToTop: true }},
+    { path: '/Info_Detail', component: Info_DetailComponent , meta: { scrollToTop: true }},
+    { path: '/Info_Post', component: Info_PostComponent , meta: { scrollToTop: true }},
+    { path: '/Review', component: ReviewComponent , meta: { scrollToTop: true }},
+    { path: '/Review_Detail', component: Review_DetailComponent , meta: { scrollToTop: true }},
+    { path: '/Review_Post', component: Review_PostComponent , meta: { scrollToTop: true }},
+    { path: '/Cmnty', component: CmntyComponent , meta: { scrollToTop: true }},
+    { path: '/Notice', component: NoticeComponent , meta: { scrollToTop: true }},
+  ]
 })
-
-// const router = new VueRouter({
-//     mode: "history",
-//     base: process.env.BASE_URL,
-//     routes,
-//     scrollBehavior (to, from, savedPosition) {
-//         if (savedPosition) {
-//             return savedPosition;
-//         } else if (to.hash) {
-
-//             return {
-//                 selector: to.hash,
-//             }
-//         }
-//     }
-// })
