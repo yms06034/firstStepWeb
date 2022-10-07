59bef18e4a5e0bc0a226a16f726935c44576dd26
Kim Sung Jin <yms06034@gmail.com>
2022-10-07 14:09:23 +0900

    doing Notice_Page Design

---
+++ src/routes/Notice.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p class="container">Hello World</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+p {
+  font-size: 20px;
+  font-weight: bold;
+  color: yellowgreen;
+}
+</style>
