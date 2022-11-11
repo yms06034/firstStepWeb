fab901d0e1968160e609a169ba68d9938ba5c1e2
Kim Sung Jin <yms06034@gmail.com>
2022-11-11 21:19:31 +0900

    Doing Review_Detail.pate

--- src/routes/Review_Detail.vue
+++ src/routes/Review_Detail.vue
@@ -416,7 +416,7 @@
                   </tr>
                   <tr class="r_d_info_tr">
                     <th class="r_d_info_dest">인원&가격</th>
-                    <td class="r_d_info_adres">2인 / 1박 - 45000원</td>
+                    <td class="r_d_info_adres">1인 / 1박 - 45000원</td>
                   </tr>
                 </tbody>
               </table>
